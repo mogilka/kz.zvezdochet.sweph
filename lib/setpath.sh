1c9dfe2d602861b53471015245b2d4cfee17c223
nataly <info@zvezdochet.kz>
2014-07-05 21:03:08 +0600

    Переход на Eclipse 4 SwissEph 2.0

---
+++ lib/setpath.sh
@@ -0,0 +1,1 @@
+export SE_EPHE_PATH=/home/nataly/workspace/kz.zvezdochet.sweph/lib/ephe
---
+++ src/kz/zvezdochet/sweph/preferences/PreferenceConstants.java
@@ -0,0 +1,9 @@
+package kz.zvezdochet.sweph.preferences;
+
+/**
+ * Класс, предоставляющий константы для работы страницы настроек (Preferences)
+ * @author Nataly
+ */
+public class PreferenceConstants {
+	public static final String SE_EPHE_PATH = "swephLibrary";
+}
