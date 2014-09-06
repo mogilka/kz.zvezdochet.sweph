7f96a14554208c440254660302541434de93b581
nataly <info@zvezdochet.kz>
2014-09-06 22:11:28 +0600

    Ссылка на библиотеку прописывается через систему плагинов

--- src/kz/zvezdochet/sweph/preferences/PreferenceConstants.java
+++
@@ -1,9 +0,0 @@
-package kz.zvezdochet.sweph.preferences;
-
-/**
- * Класс, предоставляющий константы для работы страницы настроек (Preferences)
- * @author Nataly Didenko
- */
-public class PreferenceConstants {
-	public static final String SE_EPHE_PATH = "swephLibrary";
-}
